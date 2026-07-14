59d8c4896f4f78ad1552df5bcbf585dd285e7111
dustinober1 <106895471+dustinober1@users.noreply.github.com>
2026-07-14 16:17:09 -0400

    feat: upgrade Novel Forge project metadata

---
+++ src/application/version.ts
@@ -0,0 +1,13 @@
+import { stringifyYaml } from "../infrastructure/yaml.js";
+import { readProject } from "../project/store.js";
+import { applyGuidedProjectEvent } from "./handoff.js";
+import { NOVEL_FORGE_VERSION, versionFindings, type VersionFinding } from "./version-core.js";
+
+export { NOVEL_FORGE_VERSION, versionFindings, type VersionFinding };
+
+export function upgradeProjectVersion(root: string): string {
+  const project = structuredClone(readProject(root));
+  project.novel_forge_version = NOVEL_FORGE_VERSION;
+  applyGuidedProjectEvent(root, [{ path: "PROJECT.yaml", content: stringifyYaml(project) }], "Novel Forge: upgrade project metadata", { lastAction: `Upgraded project metadata to ${NOVEL_FORGE_VERSION}` });
+  return NOVEL_FORGE_VERSION;
+}
